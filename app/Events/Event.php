d5f1cfe2d9779bd856848700916f3217b593b2a2
Pedro <pedr0.miguel@hotmail.com>
2018-03-26 20:47:47 +0100

    initial checkin

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace lsapp\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace lsapp\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
